0ac433572f1b8575a06c48fbf28b0f82508db0f5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-29 18:49:46 +0530

    updated tallscreen

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,10 @@ module.exports = {
   content: ["./src/index.html"],
   theme: {
     extend: {
-      screens:{
-        widescreen:{
-          'raw':'(min-aspect-ratio:3/2)'
-        },
-        tallscreen:{
-          'raw':'(min-aspect-ratio:1/2)'
-        },
-      }
+      screens: {
+        widescreen: { raw: "(min-aspect-ratio:3/2)" },
+        tallscreen: { raw: "(min-aspect-ratio:13/20)" },
+      },
     },
   },
   plugins: [],
